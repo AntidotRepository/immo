000c62181b89a60a4cc2bd868162f23c8bce03a1
AntidotRepository <haroldducept@gmail.com>
2019-12-21 18:46:47 +0100

    Add skip_parse option

--- Controller/controller.py
+++ Controller/controller.py
@@ -9,9 +9,10 @@ import urllib.request
 import gmplot
 from geopy.geocoders import Nominatim
 
+
 class Controller:
 
-    def __init__(self, fake=False):
+    def __init__(self, fake=False, skip_parse=False):
         self.my_parser = Immo24_Parser()
         self.my_db = DB()
         self.offers = list()
@@ -21,40 +22,13 @@ class Controller:
                 getattr(ssl, '_create_unverified_context', None)):
             ssl._create_default_https_context = \
                 ssl._create_unverified_context
-
-        if fake is True:
-            # For test purpose only
-            # Store the HTML file as a string
-            page = open('toParse.html', 'r')
-            dict_offers = self.my_parser.get_offers(page)
-
-            for dict_offer in dict_offers:
-                dict_offer['stillAvailable'] = True
-                dict_offer['lastTimeView'] = time.strftime('%Y-%m-%d %H:%M:%S')
-                offer = Offer(dict_offer)
-                self.offers.append(offer)
-                self.my_db.insert_offer(offer)
-            dict_offers.clear()
-
-        else:
-            # Normal use!
-            # First, check how many page we have to retrieve.
-            page = urllib.request.urlopen('https://www.immobilienscout24.de/' +
-                                          'Suche/de/berlin/berlin/wohnung-' +
-                                          'kaufen?pagenumber=1')
-            print("Get number of pages")
-            self.number_of_pages = self.my_parser.get_number_of_pages(page)
-
-            # For each page, get the offers
-            for i in range(self.number_of_pages):
-                print("page {}/{}".format(i, self.number_of_pages))
-                page = urllib.request.urlopen('https://www.immobilienscout24' +
-                                              '.de/Suche/de/berlin/berlin/' +
-                                              'wohnung-kaufen?pagenumber={}'.
-                                              format(i))
+        if skip_parse is False:
+            if fake is True:
+                # For test purpose only
+                # Store the HTML file as a string
+                page = open('toParse.html', 'r')
                 dict_offers = self.my_parser.get_offers(page)
 
-                # Store each offer in the DB
                 for dict_offer in dict_offers:
                     dict_offer['stillAvailable'] = True
                     dict_offer['lastTimeView'] = time.strftime('%Y-%m-%d %H:%M:%S')
@@ -62,6 +36,33 @@ class Controller:
                     self.offers.append(offer)
                     self.my_db.insert_offer(offer)
                 dict_offers.clear()
+
+            else:
+                # Normal use!
+                # First, check how many page we have to retrieve.
+                page = urllib.request.urlopen('https://www.immobilienscout24.de/' +
+                                              'Suche/de/berlin/berlin/wohnung-' +
+                                              'kaufen?pagenumber=1')
+                print("Get number of pages")
+                self.number_of_pages = self.my_parser.get_number_of_pages(page)
+
+                # For each page, get the offers
+                for i in range(self.number_of_pages):
+                    print("page {}/{}".format(i, self.number_of_pages))
+                    page = urllib.request.urlopen('https://www.immobilienscout24' +
+                                                  '.de/Suche/de/berlin/berlin/' +
+                                                  'wohnung-kaufen?pagenumber={}'.
+                                                  format(i))
+                    dict_offers = self.my_parser.get_offers(page)
+
+                    # Store each offer in the DB
+                    for dict_offer in dict_offers:
+                        dict_offer['stillAvailable'] = True
+                        dict_offer['lastTimeView'] = time.strftime('%Y-%m-%d %H:%M:%S')
+                        offer = Offer(dict_offer)
+                        self.offers.append(offer)
+                        self.my_db.insert_offer(offer)
+                    dict_offers.clear()
 
 	# Map stuffs
         gelocator = Nominatim(user_agent="lol")
